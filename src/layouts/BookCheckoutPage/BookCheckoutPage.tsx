9093a445877ab77dce452fdb032c9f9ded5a7e5c
Lachezar Ivanov <128148925+LachezarIvanovv@users.noreply.github.com>
2024-02-13 20:43:01 +0200

    Added rating stars under every book

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { StartsReview } from "../Utils/StarsReview";
 
 export const BookCheckoutPage = () => {
   const [book, setBook] = useState<BookModel>();
@@ -77,6 +78,7 @@ export const BookCheckoutPage = () => {
               <h2>{book?.title}</h2>
               <h5 className="text-primary">{book?.author}</h5>
               <p className="lead">{book?.description}</p>
+              <StartsReview rating={4.5} size={32} />
             </div>
           </div>
         </div>
@@ -100,9 +102,10 @@ export const BookCheckoutPage = () => {
             <h2>{book?.title}</h2>
             <h5 className="text-primary">{book?.author}</h5>
             <p className="lead">{book?.description}</p>
+            <StartsReview rating={4.5} size={32} />
           </div>
         </div>
-        <hr />
+          <hr />
       </div>
     </div>
   );
